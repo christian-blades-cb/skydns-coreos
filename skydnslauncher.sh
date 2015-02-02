76e042c1be342c217ea7841cd854cbc670c1de55
Christian <christian.blades@careerbuilder.com>
2015-02-02 15:24:38 -0500

    Wrong env, I needed ETCD_MACHINES instead of ETCD_HOSTS

--- skydnslauncher.sh
+++ skydnslauncher.sh
@@ -7,7 +7,7 @@
 if [ -z "$ETCD_HOSTS" ]; then
     docker_host=$(ip route | grep default | cut -d' ' -f3)
     >&2 echo "Host found at ${docker_host}"
-    export ETCD_HOSTS="http://${docker_host}:4001"    
+    export ETCD_MACHINES="http://${docker_host}:4001"
 fi
 
 skydns $@
